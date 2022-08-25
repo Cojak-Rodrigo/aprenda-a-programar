8df6fccba0bee4f243302354571fe9f1eec7507f
Cojak-Rodrigo <rodrigo_cojak@msn.com>
2022-08-25 01:25:41 -0300

    primeira programação em js.

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,6 +8,14 @@ let idade
 idade = 36
 let lugar
 lugar = "América do Norte"
-console.log(nome,email,profissão,idade,lugar)
- 
+let ola
+ola = 'Olá'
+let mensagemDaAgencia
+mensagemDaAgencia = 'Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para'
+let informaçãoIdade = 'Confirma pra gente alguns dados? A sua idade é'
+let informaçãoProfissão = 'você é'
+let informaçãoEmail = 'e o seu email é'
+let Agradecimento = 'Ficamos no aguardo. Muito obrigado e boa viagem! :D'
+console.log(ola,nome,mensagemDaAgencia,lugar,informaçãoIdade,idade,informaçãoProfissão,profissão,informaçãoEmail,email,Agradecimento)
+
 
